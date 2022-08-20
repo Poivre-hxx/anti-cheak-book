ec3a376e56af5fe6832ad914290ec850b51767dd
poivre <poivre0101@gmail.com>
2022-08-21 06:41:35 +0800

    feat(css): add cover css

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,8 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['import', {libraryName: '@ant-design/react-native'}], // 与 Web 平台的区别是不需要设置 style
+    ],
   };
 };
