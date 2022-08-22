a07b17e8bf14523d7d594b417c5972f220098a1a
poivre <poivre0101@gmail.com>
2022-08-22 11:22:08 +0800

    feat(state): fix checkbox bug

--- src/pages/exam/examPage.js
+++ src/pages/exam/examPage.js
@@ -1,65 +1,25 @@
 import React from "react";
 import { StyleSheet, View, ScrollView, ImageBackground } from "react-native";
-import { List, Flex, Text, Radio, Button } from "@ant-design/react-native";
+import { List, Flex, Text, Checkbox, Button } from "@ant-design/react-native";
 import msg from "./msg.json";
 
 const Item = List.Item;
 
-// 获取随机数
-function ran() {
-  return Math.floor(Math.random() * 12);
-}
-const value = ran();
-const SquareUP = (props) => {
-  const style = {
+const styles = StyleSheet.create({
+  squareUp: {
     borderRadius: 15,
     backgroundColor: "#fff",
     width: 308,
     height: 218,
     marginTop: 420,
-  };
-  return (
-    <View style={style}>
-      <List>
-        <Item>题目</Item>
-      </List>
-      <Text style={styles.title}>{msg[value].title}</Text>
-    </View>
-  );
-};
-
-const SquareDown = (props) => {
-  const style = {
+  },
+  squareDown: {
     borderRadius: 15,
     backgroundColor: "#fff",
     width: 308,
     height: 283,
     marginTop: 50,
-  };
-  return (
-    <View style={style}>
-      <List>
-        <Item>选项</Item>
-        <Item style={styles.ans} thumb={<Radio defaultChecked={false}>A:{msg[value].A}</Radio>} />
-        <Item style={styles.ans} thumb={<Radio defaultChecked={false}>B:{msg[value].B}</Radio>} />
-        <Item style={styles.ans} thumb={<Radio defaultChecked={false}>C:{msg[value].C}</Radio>} />
-        <Item style={styles.ans} thumb={<Radio defaultChecked={false}>D:{msg[value].D}</Radio>} />
-      </List>
-    </View>
-  );
-};
-
-function reRend() {
-  ran();
-  return (
-    <View>
-      <SquareUP />
-      <SquareDown />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
+  },
   title: {
     margin: 5,
     fontSize: 15,
@@ -81,25 +41,83 @@ const styles = StyleSheet.create({
   },
 });
 
-function ExamPage({ navigation }) {
-  return (
-    <ScrollView style={{ backgroundColor: "#eef1f2" }}>
-      <Flex justify="center">
-        <ImageBackground
-          style={styles.blue}
-          source={require("../../assets/imgs/blue.png")}
-        >
-          <Flex justify="center" direction="column">
-            <SquareUP />
-            <SquareDown />
-            <Button style={styles.button} onPress={() => reRend()}>
-              确认
-            </Button>
-          </Flex>
-        </ImageBackground>
-      </Flex>
-    </ScrollView>
-  );
-}
+export default class ExamPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: Math.floor(Math.random() * 12),
+      status: false,
+    };
+  }
 
-export default ExamPage;
+  handleClick = () => {
+    this.setState({
+      value: Math.floor(Math.random() * 12),
+      status: false,
+    });
+  };
+
+  render() {
+    return (
+      <ScrollView style={{ backgroundColor: "#eef1f2" }}>
+        <Flex justify="center">
+          <ImageBackground
+            style={styles.blue}
+            source={require("../../assets/imgs/blue.png")}
+          >
+            <Flex justify="center" direction="column">
+              {/* // 上半部分 */}
+              <View style={styles.squareUp}>
+                <List>
+                  <Item>题目</Item>
+                </List>
+                <Text style={styles.title}>{msg[this.state.value].title}</Text>
+              </View>
+              {/* // 下半部分 */}
+              <View style={styles.squareDown}>
+                <List>
+                  <Item>选项</Item>
+                  <Item
+                    style={styles.ans}
+                    thumb={
+                      <Checkbox key={Math.random()} defaultChecked={this.state.status}>
+                        A:{msg[this.state.value].A}
+                      </Checkbox>
+                    }
+                  />
+                  <Item
+                    style={styles.ans}
+                    thumb={
+                      <Checkbox key={Math.random()} defaultChecked={this.state.status}>
+                        B:{msg[this.state.value].B}
+                      </Checkbox>
+                    }
+                  />
+                  <Item
+                    style={styles.ans}
+                    thumb={
+                      <Checkbox key={Math.random()} defaultChecked={this.state.status}>
+                        C:{msg[this.state.value].C}
+                      </Checkbox>
+                    }
+                  />
+                  <Item
+                    style={styles.ans}
+                    thumb={
+                    <Checkbox key={Math.random()} defaultChecked={this.state.status}>
+                        D:{msg[this.state.value].D}
+                      </Checkbox>
+                    }
+                  />
+                </List>
+              </View>
+              <Button style={styles.button} onPress={this.handleClick}>
+                确认
+              </Button>
+            </Flex>
+          </ImageBackground>
+        </Flex>
+      </ScrollView>
+    );
+  }
+}
