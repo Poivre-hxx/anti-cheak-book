5cdbc6908bb2606375da265c9162230ac8a122af
Xav1erSue <s791521574@live.com>
2022-09-16 19:49:31 +0800

    feat: history page

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -1,7 +1,9 @@
 import { getUserInfo } from "@/api/user";
-import { Button, Flex, Text, View } from "@ant-design/react-native";
+import { List, Text, View } from "@ant-design/react-native";
 import { useState, useEffect } from "react";
 import { ScrollView } from "react-native";
+import dayjs from "dayjs";
+const Item = List.Item;
 
 const History = ({ navigation }) => {
   const [examHistory, setExamHistory] = useState([]);
@@ -18,25 +20,59 @@ const History = ({ navigation }) => {
 
   if (!loaded) return;
   return (
-    <ScrollView style={{ backgroundColor: "#eef1f2" }}>
-      <Flex direction="cols" align="center" justify="center">
+    <ScrollView
+      style={{ flex: 1, backgroundColor: "#f5f5f9" }}
+      automaticallyAdjustContentInsets={false}
+      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}
+    >
+      <List renderHeader={"考试记录"}>
         {examHistory.map((exam, index) => (
-          <View key={index}>
-            <Text>{exam.uid}</Text>
-            <Text>{exam.score}分</Text>
-            <Button
-              onPress={() => {
-                navigation.navigate("Mistakes", {
-                  data: {
-                    paper: exam.paper,
-                    mistakes: exam.mistakes,
-                  },
-                });
+          <Item
+            key={index}
+            extra={
+              <View
+                style={{
+                  padding: 10,
+                  width: 60,
+                  height: 60,
+                  backgroundColor: "#74D78F",
+                  borderRadius: "50%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={{ fontSize: 20, color: "white" }}>
+                  {exam.score}分
+                </Text>
+              </View>
+            }
+            onPress={() => {
+              navigation.navigate("Mistakes", {
+                data: {
+                  paper: exam.paper,
+                  mistakes: exam.mistakes,
+                },
+              });
+            }}
+          >
+            <View
+              style={{
+                margin: 5,
               }}
-            ></Button>
-          </View>
+            >
+              <Text style={{ paddingVertical: 0 }}>
+                时间：
+                {dayjs(exam.createdAt).format("YYYY年M月D日 HH:mm:ss")}
+              </Text>
+              <Text style={{ paddingTop: 4, fontSize: 12, color: "#A8A9AC" }}>
+                {exam.uid}
+              </Text>
+            </View>
+          </Item>
         ))}
-      </Flex>
+      </List>
     </ScrollView>
   );
 };
